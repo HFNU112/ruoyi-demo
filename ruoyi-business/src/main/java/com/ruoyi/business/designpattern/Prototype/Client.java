5b859018aee125cf8b539d4f0ee2439c5419c788
hfnu_2010211112 <2942885033@qq.com>
2023-11-13 20:38:01 +0800

    feat:原型模式

---
+++ ruoyi-business/src/main/java/com/ruoyi/business/designpattern/Prototype/Client.java
@@ -0,0 +1,11 @@
+package com.ruoyi.business.designpattern.Prototype;
+
+/**
+ * @Author Husp
+ * @Date 2023/11/7 22:47
+ */
+public class Client {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ ruoyi-business/src/main/java/com/ruoyi/business/designpattern/Prototype/Sheep.java
@@ -0,0 +1,8 @@
+package com.ruoyi.business.designpattern.Prototype;
+
+/**
+ * @Author Husp
+ * @Date 2023/11/7 22:28
+ */
+public class Sheep implements Cloneable{
+}
